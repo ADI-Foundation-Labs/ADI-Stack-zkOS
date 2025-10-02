b4262817e9649255a7308c55dc6d53394a35d3a6
Mario A. Barbara <mariobarbara@protonmail.ch>
2025-10-02 19:46:35 +0200

    updated keccak abi: less rotations + removed final xor bloat

--- crypto/src/sha3/test_program/dump_bin.sh
+++ crypto/src/sha3/test_program/dump_bin.sh
@@ -4,6 +4,14 @@ set -e
 
 # cargo objdump --features keccak_f1600_test --release --target riscv32i-unknown-none-elf -v -- -d
 
+rustup target add riscv32i-unknown-none-elf
+rustup component add llvm-tools 
+
+rustup component add llvm-tools-preview
+cargo install cargo-binutils
+
+# for compilation, target riscv32 is already set by .cargo folder
+cargo update
 cargo objcopy --release --features keccak_f1600_test -- -O binary app_keccak_simple.bin
 cargo objcopy --release --features bad_keccak_f1600_test -- -O binary app_keccak_bad.bin
 cargo objcopy --release --features hash_chain_test -- -O binary app_keccak_bench.bin
