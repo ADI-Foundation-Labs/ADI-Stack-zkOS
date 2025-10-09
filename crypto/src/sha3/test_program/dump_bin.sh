a1cc35da491bf4d8ea4c83ba45275d30e62dded1
Mario A. Barbara <mariobarbara@protonmail.ch>
2025-10-10 00:28:03 +0200

    sha3: add .text to dump_bin.sh

--- crypto/src/sha3/test_program/dump_bin.sh
+++ crypto/src/sha3/test_program/dump_bin.sh
@@ -12,7 +12,13 @@ cargo install cargo-binutils
 
 # for compilation, target riscv32 is already set by .cargo folder
 cargo update
+
 cargo objcopy --release --features keccak_f1600_test -- -O binary app_keccak_simple.bin
 cargo objcopy --release --features bad_keccak_f1600_test -- -O binary app_keccak_bad.bin
 cargo objcopy --release --features hash_chain_test -- -O binary app_keccak_bench.bin
 cargo objcopy --release --features mini_digest_test -- -O binary app_keccak_complex.bin
+
+cargo objcopy --release --features keccak_f1600_test -- -O binary --only-section=.text app_keccak_simple.text
+cargo objcopy --release --features bad_keccak_f1600_test -- -O binary --only-section=.text app_keccak_bad.text
+cargo objcopy --release --features hash_chain_test -- -O binary --only-section=.text app_keccak_bench.text
+cargo objcopy --release --features mini_digest_test -- -O binary --only-section=.text app_keccak_complex.text
